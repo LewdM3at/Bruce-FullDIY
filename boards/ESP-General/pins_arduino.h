403f643e0170c6bb15363b87fcc36e1f9b78a255
LewdM3at <angkunster@hotmail.com>
2025-12-03 11:52:10 +0100

    Changes the pinouts of the board to match the daemon project

--- boards/ESP-General/pins_arduino.h
+++ boards/ESP-General/pins_arduino.h
@@ -1,69 +1,122 @@
 #ifndef Pins_Arduino_h
 #define Pins_Arduino_h
 
-#include <stdint.h>
 #include "soc/soc_caps.h"
+#include <stdint.h>
 
-#define USB_VID 0x303a
-#define USB_PID 0x1001
+static const uint8_t TX = 1;
+static const uint8_t RX = 2;
 
-// Some boards have too low voltage on this pin (board design bug)
-// Use different pin with 3V and connect with 48
-// and change this setup for the chosen pin (for example 38)
-#define PIN_RGB_LED 48
-// BUILTIN_LED can be used in new Arduino API digitalWrite() like in Blink.ino
-static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT + PIN_RGB_LED;
-#define BUILTIN_LED LED_BUILTIN  // backward compatibility
-#define LED_BUILTIN LED_BUILTIN  // allow testing #ifdef LED_BUILTIN
-// RGB_BUILTIN and RGB_BRIGHTNESS can be used in new Arduino API rgbLedWrite()
-#define RGB_BUILTIN    LED_BUILTIN
-#define RGB_BRIGHTNESS 64
+static const uint8_t SDA = 47;
+static const uint8_t SCL = 48;
 
-static const uint8_t TX = 43;
-static const uint8_t RX = 44;
+// Modified elsewhere
+static const uint8_t SS = 3;
+static const uint8_t MOSI = 17;
+static const uint8_t MISO = 8;
+static const uint8_t SCK = 18;
 
-static const uint8_t SDA = 8;
-static const uint8_t SCL = 9;
+#define SERIAL_RX 2
+#define SERIAL_TX 1
+#define BAD_RX SERIAL_RX
+#define BAD_TX SERIAL_TX
+#define USB_as_HID 1
 
-static const uint8_t SS = 10;
-static const uint8_t MOSI = 11;
-static const uint8_t MISO = 13;
-static const uint8_t SCK = 12;
+#define BTN_ALIAS "\"OK\""
+#define HAS_3_BUTTONS
+#define SEL_BTN 0
+#define UP_BTN 41
+#define DW_BTN 40
+#define BTN_ACT LOW
 
-static const uint8_t A0 = 1;
-static const uint8_t A1 = 2;
-static const uint8_t A2 = 3;
-static const uint8_t A3 = 4;
-static const uint8_t A4 = 5;
-static const uint8_t A5 = 6;
-static const uint8_t A6 = 7;
-static const uint8_t A7 = 8;
-static const uint8_t A8 = 9;
-static const uint8_t A9 = 10;
-static const uint8_t A10 = 11;
-static const uint8_t A11 = 12;
-static const uint8_t A12 = 13;
-static const uint8_t A13 = 14;
-static const uint8_t A14 = 15;
-static const uint8_t A15 = 16;
-static const uint8_t A16 = 17;
-static const uint8_t A17 = 18;
-static const uint8_t A18 = 19;
-static const uint8_t A19 = 20;
+#define RXLED 4
+#define LED 5
+#define LED_ON HIGH
+#define LED_OFF LOW
 
-static const uint8_t T1 = 1;
-static const uint8_t T2 = 2;
-static const uint8_t T3 = 3;
-static const uint8_t T4 = 4;
-static const uint8_t T5 = 5;
-static const uint8_t T6 = 6;
-static const uint8_t T7 = 7;
-static const uint8_t T8 = 8;
-static const uint8_t T9 = 9;
-static const uint8_t T10 = 10;
-static const uint8_t T11 = 11;
-static const uint8_t T12 = 12;
-static const uint8_t T13 = 13;
-static const uint8_t T14 = 14;
+#define USE_CC1101_VIA_SPI
+#define CC1101_GDO0_PIN 9
+#define CC1101_GDO2_PIN 10
+#define CC1101_SS_PIN 46
+#define CC1101_MOSI_PIN SPI_MOSI_PIN
+#define CC1101_SCK_PIN SPI_SCK_PIN
+#define CC1101_MISO_PIN SPI_MISO_PIN
 
+#define USE_NRF24_VIA_SPI
+#define NRF24_CE_PIN 21
+#define NRF24_SS_PIN 14
+#define NRF24_MOSI_PIN SPI_MOSI_PIN
+#define NRF24_SCK_PIN SPI_SCK_PIN
+#define NRF24_MISO_PIN SPI_MISO_PIN
+
+#define FP 1
+#define FM 2
+#define FG 3
+
+#define HAS_SCREEN 1
+#define ROTATION 3
+//#define MINBRIGHT (uint8_t)1
+
+#define USER_SETUP_LOADED 1
+#define ILI9488_DRIVER 1
+#define TFT_RGB_ORDER 0
+#define TFT_WIDTH 320
+#define TFT_HEIGHT 480
+#define TFT_BACKLIGHT_ON 1
+//#define TFT_BL 6
+#define TFT_RST 16
+#define TFT_DC 15
+#define TFT_MISO 8
+#define TFT_MOSI 17
+#define TFT_SCLK 18
+#define TFT_CS 7
+#define TOUCH_CS -1 // SDCARD_CS to make sure SDCard works
+#define SMOOTH_FONT 1
+#define SPI_FREQUENCY 40000000
+#define SPI_READ_FREQUENCY 20000000
+#define SPI_TOUCH_FREQUENCY 2500000
+
+#define SDCARD_CS 3
+#define SDCARD_SCK 18
+#define SDCARD_MISO 8
+#define SDCARD_MOSI 17
+
+#define GROVE_SDA 47
+#define GROVE_SCL 48
+
+#define SPI_SCK_PIN 13
+#define SPI_MOSI_PIN 12
+#define SPI_MISO_PIN 11
+#define SPI_SS_PIN 43
+
+// RGB LED
+
+//#define HAS_RGB_LED 1
+//#define RGB_LED 45
+//#define LED_TYPE WS2812B
+//#define LED_ORDER GRB
+//#define LED_TYPE_IS_RGBW 0
+//#define LED_COUNT 16
+
+//#define LED_COLOR_STEP 15
+
+//#define XPOWERS_CHIP_BQ25896
+
+// USE BOOST ENABLE PMIC 5V OUTPUT
+//#define USE_BOOST
+
+
+// Mic#
+#define PIN_CLK 1
+#define PIN_DATA 10
+#define PIN_WS 2
+
+// IO EXPANDER
+//#define USE_IO_EXPANDER
+//#define IO_EXPANDER_AW9523
+//#define IO_EXP_GPS 13
+//#define IO_EXP_MIC 4
+//#define IO_EXP_VIBRO 2
+//#define IO_EXP_CC_RX 7
+//#define IO_EXP_CC_TX 12
 #endif /* Pins_Arduino_h */
