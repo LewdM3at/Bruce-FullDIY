4e8e5f2429f7dad8cb34cedb6e1131c99ecf6d31
LewdM3at <angkunster@hotmail.com>
2025-12-03 11:50:56 +0100

    Added setup_gpio settings and input handler logic

--- boards/ESP-General/interface.cpp
+++ boards/ESP-General/interface.cpp
@@ -1,12 +1,36 @@
 #include "core/powerSave.h"
 #include <interface.h>
+#include <Wire.h>
 
 /***************************************************************************************
 ** Function name: _setup_gpio()
 ** Location: main.cpp
 ** Description:   initial setup for the device
 ***************************************************************************************/
-void _setup_gpio() { bruceConfig.startupApp = "WebUI"; }
+void _setup_gpio()
+{
+    pinMode(UP_BTN, INPUT_PULLUP);
+    pinMode(SEL_BTN, INPUT_PULLUP);
+    pinMode(DW_BTN, INPUT_PULLUP);
+
+    Wire.setPins(GROVE_SDA, GROVE_SCL);
+    Wire.begin(GROVE_SDA, GROVE_SCL);
+
+    pinMode(NRF24_SS_PIN, OUTPUT);
+    pinMode(CC1101_SS_PIN, OUTPUT);
+    pinMode(SDCARD_CS, OUTPUT);
+    pinMode(TFT_CS, OUTPUT);
+
+    digitalWrite(NRF24_SS_PIN, HIGH);
+    digitalWrite(CC1101_SS_PIN, HIGH);
+    digitalWrite(SDCARD_CS, HIGH);
+    digitalWrite(TFT_CS, HIGH);
+
+    bruceConfig.rfModule = CC1101_SPI_MODULE;
+    bruceConfig.rfidModule = PN532_I2C_MODULE;
+    bruceConfig.irRx = RXLED;
+    bruceConfig.startupApp = "WebUI";
+}
 
 /***************************************************************************************
 ** Function name: getBattery()
@@ -32,7 +56,25 @@ void _setBrightness(uint8_t brightval) {}
 ** Function: InputHandler
 ** Handles the variables PrevPress, NextPress, SelPress, AnyKeyPress and EscPress
 **********************************************************************/
-void InputHandler(void) {}
+void InputHandler(void)
+{
+    static unsigned long tm = 0;
+    if (millis() - tm < 200 && !LongPress) return;
+
+    bool upPressed = (digitalRead(UP_BTN) == LOW);
+    bool selPressed = (digitalRead(SEL_BTN) == LOW);
+    bool dwPressed = (digitalRead(DW_BTN) == LOW);
+
+    bool anyPressed = upPressed || selPressed || dwPressed;
+    if (anyPressed) tm = millis();
+    if (anyPressed && wakeUpScreen()) return;
+
+    AnyKeyPress = anyPressed;
+    PrevPress = upPressed;
+    EscPress = upPressed;
+    NextPress = dwPressed;
+    SelPress = selPressed;
+}
 
 /*********************************************************************
 ** Function: powerOff
